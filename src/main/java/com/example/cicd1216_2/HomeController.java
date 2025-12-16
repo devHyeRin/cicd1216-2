83d227328dd4d20dd29bbbd17188ad775bbc0d3e
devHyeRin <hyerinlee0v0@gmail.com>
2025-12-16 11:25:30 +0900

    cicd2

---
+++ src/main/java/com/example/cicd1216_2/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.cicd1216_2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hicicd")
+    public String hicicd(){
+        return "hicicd 1216 ^^^^^";
+    }
+
+    @GetMapping("/hicicd2")
+    public String hicicd2(){
+        return "새로운 개발 ^^^^^";
+    }
+
+    @GetMapping("/hicicd3")
+    public String hicicd3(){
+        return "새로운 개발 cicd 적용 ^^^^^";
+    }
+}
